10df778dfbe7cc4fc7c66d90f51adfc35bcc5f5a
fuzzy-tribble <fancy-broccoli@pm.me>
2025-05-05 12:19:41 -0400

    updating namespace

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setuptools.setup(
-    name="nbconvertjkl-nancynobody",
+    name="nbconvertjkl-fuzzy-tribble",
     version="0.0.1",
-    author="nancynobody",
+    author="fuzzy-tribble",
     # author_email="author@example.com",
     description="A small package to convert ipynbs to jekyll compatable for GitHub pages",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/nancynobody/python3_fluency",
+    url="https://github.com/fuzzy-tribble/python3_fluency",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
